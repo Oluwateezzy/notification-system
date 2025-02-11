3c9ef752f0b371d214adf7f7c85fd69d68766855
Oluwatobiloba <oluwateezzy03@gmail.com>
2025-02-11 01:42:02 +0100

    add Notification module and Prisma service; update app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { NotificationModule } from './notification/notification.module';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, NotificationModule, RedisModule],
   controllers: [AppController],
   providers: [AppService],
 })
